0bb410880dbc6227fb29e7780e1d3f475a538bb2
elite <elite@elite-ui-MacBook-Pro-4.local>
2016-01-03 11:42:48 +0900

    특수문자나 문자열 삽입이 필요 없는 경우는 작은따옴표를 사용하는 것이 좋다

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -5,17 +5,17 @@ class PostsControllerTest < ActionDispatch::IntegrationTest
     @post = posts(:one)
   end
 
-  test "should get index" do
+  test 'should get index' do
     get posts_url
     assert_response :success
   end
 
-  test "should get new" do
+  test 'should get new' do
     get new_post_url
     assert_response :success
   end
 
-  test "should create post" do
+  test 'should create post' do
     assert_difference('Post.count') do
       post posts_url, params: { post: { body: @post.body, title: @post.title } }
     end
@@ -23,17 +23,17 @@ class PostsControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to post_path(Post.last)
   end
 
-  test "should show post" do
+  test 'should show post' do
     get post_url(@post)
     assert_response :success
   end
 
-  test "should get edit" do
+  test 'should get edit' do
     get edit_post_url(@post)
     assert_response :success
   end
 
-  test "should update post" do
+  test 'should update post' do
     patch post_url(@post), params: { 
 				    post: { 
 					   body: @post.body, 
@@ -43,7 +43,7 @@ class PostsControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to post_path(@post)
   end
 
-  test "should destroy post" do
+  test 'should destroy post' do
     assert_difference('Post.count', -1) do
       delete post_url(@post)
     end
