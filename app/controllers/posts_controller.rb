638276de57f06a51500313f2ade0fe734298923b
elite <elite@elite-ui-MacBook-Pro-4.local>
2016-01-03 11:23:39 +0900

    한 행에는 80자까지만 쓴다

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -28,7 +28,9 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
+        format.html { 
+		redirect_to @post, notice: 'Post was successfully created.' 
+	}
         format.json { render :show, status: :created, location: @post }
       else
         format.html { render :new }
@@ -42,7 +44,9 @@ class PostsController < ApplicationController
   def update
     respond_to do |format|
       if @post.update(post_params)
-        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
+        format.html { 
+		redirect_to @post, notice: 'Post was successfully updated.' 
+	}
         format.json { render :show, status: :ok, location: @post }
       else
         format.html { render :edit }
@@ -56,7 +60,9 @@ class PostsController < ApplicationController
   def destroy
     @post.destroy
     respond_to do |format|
-      format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
+      format.html { 
+		redirect_to posts_url, notice: 'Post was successfully destroyed.' 
+      }
       format.json { head :no_content }
     end
   end
@@ -67,7 +73,8 @@ class PostsController < ApplicationController
       @post = Post.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
+    # Never trust parameters from the scary internet, 
+    # only allow the white list through.
     def post_params
       params.require(:post).permit(:title, :body)
     end
