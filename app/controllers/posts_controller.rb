7a828e71237b596806e6c3de453ebcaabcdf315b
elite <elite@elite-ui-MacBook-Pro-4.local>
2016-01-03 11:47:59 +0900

    public, protexted, private은 메소드 정의와 같은 레벨로 들여쓰기 한다

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -71,13 +71,13 @@ class PostsController < ApplicationController
   private
 
     # Use callbacks to share common setup or constraints between actions.
-    def set_post
+  def set_post
       @post = Post.find(params[:id])
-    end
+  end
 
     # Never trust parameters from the scary internet, 
     # only allow the white list through.
-    def post_params
+  def post_params
       params.require(:post).permit(:title, :body)
-    end
+  end
 end
