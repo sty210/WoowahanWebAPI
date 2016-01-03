04c851549fd22317ad0341eb8e0f16976f8035db
elite <elite@elite-ui-MacBook-Pro-4.local>
2016-01-03 14:58:10 +0900

    게시물 목록 얻기 API제작 - 1,2,3번 테스트 작성 - Jbuilder 사용해보기

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,5 +1,7 @@
 module Api
   class PostsController < ApplicationController
+    respond_to :json
+
     def index
       @posts = Post.all
       respond_with(@posts)
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  # Serve websocket cable requests in-process
+  # mount ActionCable.server => '/cable'
+
   namespace :api do
     resources :posts, only: :index
   end
-
-  # Serve websocket cable requests in-process
-  # mount ActionCable.server => '/cable'
 end
